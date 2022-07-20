15b63e09fde1f3a4754b436fdaf94aa4ae43dbbb
Gong <gong@userleap.com>
2022-07-20 16:06:17 -0700

    initial implementation

--- Example/BasicExample/BasicExample/BasicExampleApp.swift
+++ Example/BasicExample/BasicExample/BasicExampleApp.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Segment
+import SegmentSprig
 
 @main
 struct BasicExampleApp: App {
@@ -22,7 +23,7 @@ extension Analytics {
         let analytics = Analytics(configuration: Configuration(writeKey: "<YOUR WRITE KEY>")
                     .flushAt(3)
                     .trackApplicationLifecycleEvents(true))
-        //analytics.add(plugin: <Destination>())
+        analytics.add(plugin: SprigDestination())
         return analytics
     }
 }

dab48fda6aad83ed898297b596963fba9bad612c
Gong <gong@userleap.com>
2022-11-10 15:15:00 -0800

    update ios sdk dependency to 4.11.1-alpha

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -27,7 +27,7 @@ let package = Package(
         .package(
             name: "UserLeapKit",
             url: "https://github.com/UserLeap/userleap-ios-sdk-releases/",
-            from: "4.11.0"
+            from: "4.11.1-alpha"
         )
     ],
     targets: [

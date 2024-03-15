88d835e85206181495970e16c9e4861729aa03ce
Earl Cochran <98436707+earlsprig@users.noreply.github.com>
2024-03-15 10:56:37 -0500

    Revert "Bump Sprig iOS SDK to 4.21.2 (#39)" (#40)
    This reverts commit 34bb6e1545b7c6383792cbe76effe38140eb6841.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/UserLeap/userleap-ios-sdk-releases/",
-            exact: "4.21.2"
+            exact: "4.21.1"
         )
     ],
     targets: [

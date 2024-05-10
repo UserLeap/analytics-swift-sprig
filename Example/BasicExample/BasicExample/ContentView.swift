31f562efb0cca59c8640a239d8b93c14518a0cf0
Earl Cochran <98436707+earlsprig@users.noreply.github.com>
2024-05-10 17:23:29 -0500

    Update iOS SDK to 4.21.8 (#45)
    * Bump iOS to 4.21.8. Fix test app

--- Example/BasicExample/BasicExample/ContentView.swift
+++ Example/BasicExample/BasicExample/ContentView.swift
@@ -13,44 +13,38 @@ struct ContentView: View {
         VStack {
             HStack {
                 Button(action: {
-                    Analytics.main.track(name: "Track", properties: ["age": 3, "item": "cookies"])
+                    analytics!.track(name: "Track", properties: ["age": 3, "item": "cookies"])
                 }, label: {
                     Text("Track with Props")
                 }).padding(6)
                 Button(action: {
-                    Analytics.main.track(name: "Track")
+                    analytics?.track(name: "Track")
                 }, label: {
                     Text("Track")
                 }).padding(6)
                 Button(action: {
-                    Analytics.main.screen(title: "iOS Segment Screen", properties: ["segmentActionsiOS": true, "deviceType": "iOS"])
+                    analytics?.screen(title: "iOS Segment Screen", properties: ["segmentActionsiOS": true, "deviceType": "iOS"])
                 }, label: {
                     Text("Screen with props")
                 }).padding(6)
                 Button(action: {
-                    Analytics.main.screen(title: "iOS Segment Screen")
+                    analytics?.screen(title: "iOS Segment Screen")
                 }, label: {
                     Text("Screen")
                 }).padding(6)
                 Button(action: {
-                    Analytics.main.track(name: "Signed Out")
+                    analytics?.track(name: "Signed Out")
                 }, label: {
                     Text("Signed Out")
                 }).padding(6)
             }.padding(8)
             HStack {
                 Button(action: {
-                    Analytics.main.identify(userId: "X-1234567890", traits: ["abc": 1])
+                    analytics?.identify(userId: "X-1234567890", traits: ["abc": 1])
                 }, label: {
                     Text("Identify")
                 }).padding(6)
             }.padding(8)
-        }.onAppear {
-            Analytics.main.track(name: "onAppear")
-            print("Executed Analytics onAppear()")
-        }.onDisappear {
-            Analytics.main.track(name: "onDisappear")
-            print("Executed Analytics onDisappear()")
         }
     }
 }

ea924177d02066fd9cf27c6e73667075357eedfb
Earl Cochran <earl@sprig.com>
2024-05-11 01:33:51 -0500

    Move segment write key to Config.plist

--- Example/BasicExample/BasicExample/BasicExampleApp.swift
+++ Example/BasicExample/BasicExample/BasicExampleApp.swift
@@ -26,8 +26,20 @@ struct BasicExampleApp: App {
 
 private extension BasicExampleApp {
     func setupSegment() {
-        let configuration = Configuration(writeKey: "<SEGMENT_WRITE_KEY>")
-            .flushInterval(10)
+        var config: [String: Any]?
+        if let infoPlistPath = Bundle.main.url(forResource: "Config", withExtension: "plist") {
+            do {
+                let infoPlistData = try Data(contentsOf: infoPlistPath)
+                if let dict = try PropertyListSerialization.propertyList(from: infoPlistData, options: [], format: nil) as? [String: Any] {
+                    config = dict
+                }
+            } catch {
+                print(error)
+            }
+        }
+        let segmentKey: String = config?["SEGMENT_WRITE_KEY"] as! String
+        let configuration = Configuration(writeKey: segmentKey)
+            configuration.flushAt(3)
 
         analytics = Analytics.init(configuration: configuration)
         analytics?.add(plugin: SprigDestination())

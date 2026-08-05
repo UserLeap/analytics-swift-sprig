d76cb08cebac59df306d5ca04ff02d03ceb6ee02
Kurt Hobson <kurt.hobson@sprig.com>
2026-08-05 13:06:55 -0700

    v4.34.0

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/UserLeap/userleap-ios-sdk-releases/",
-            exact: "4.33.1"
+            exact: "4.34.0"
         )
     ],
     targets: [
